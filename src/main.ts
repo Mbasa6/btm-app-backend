4d54422ee774f44ead6583c5d8488c6bad9f8327
Mbasa <Mbasa.Cabane@Mediclinic.co.za>
2025-11-09 17:23:10 +0200

    enable CORS for Expo frontend and update server listen config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:8081', 'http://10.0.2.2:8081'],
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(3000, '0.0.0.0');
+  console.log('🚀 Server running on http://10.157.1.214:3000');
 }
 bootstrap();

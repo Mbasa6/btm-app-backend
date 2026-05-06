cbf03c4de80dab35453a971ef5bf5ce6c9d9132b
Mbasa6 <mbasacabane6@gmail.com>
2026-05-06 07:25:17 +0200

    fix: import UserModule in NotificationModule for IsActiveGuard dependency

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from '../entities/notification.entity';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification])],
+  imports: [TypeOrmModule.forFeature([Notification]), UserModule],
   providers: [NotificationService],
   controllers: [NotificationController],
   exports: [NotificationService],
